cec806bfb0e88fe478c450714b3d4b5b8a7d84d2
vaiishnavitale19 <vaiishnavitale19@gmail.com>
2026-07-08 18:24:29 +0530

    Delete Program.cs

--- Program.cs
+++
@@ -1,5 +0,0 @@
-﻿        
-        Console.WriteLine("Name: Vaishnavi Tale");
-        Console.WriteLine("Branch: Computer Science and Engineering");
-        Console.WriteLine("College Name: SSGMCE");
-    
