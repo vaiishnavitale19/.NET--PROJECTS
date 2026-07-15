00a6f4ca511aea649485a86911c1a2b275b7b789
vaiishnavitale19 <vaiishnavitale19@gmail.com>
2026-07-15 15:44:00 +0530

    Delete .NET assignment problem/13july directory

--- .NET assignment problem/13july/Customer.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Customer
-{
-    public int CustomerId { get; set; }
-    public string Name { get; set; } = "";
-    public string Email { get; set; } = "";
-    public string Password { get; set; } = "";
-}
