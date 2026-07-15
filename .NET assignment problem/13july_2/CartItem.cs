38a3d65cba388244a5de47ede768e22dc1bd1da9
vaiishnavitale19 <vaiishnavitale19@gmail.com>
2026-07-15 15:44:25 +0530

    Delete .NET assignment problem/13july_2 directory

--- .NET assignment problem/13july_2/CartItem.cs
+++
@@ -1,6 +0,0 @@
-class CartItem
-{
-    public string ProductName { get; set; } = "";
-    public int Quantity { get; set; }
-    public double Price { get; set; }
-}
--- .NET assignment problem/13july_2/Product.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-class Product
-{
-    public int ProductId { get; set; }
-    public string ProductName { get; set; } = "";
-    public double Price { get; set; }
-    public int Stock { get; set; }
-}
