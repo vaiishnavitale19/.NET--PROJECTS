9c502fca129afa5c5978cde3eccee8afc2e9847f
vaiishnavitale19 <vaiishnavitale19@gmail.com>
2026-07-21 12:39:56 +0530

    Add files via upload

---
+++ .NET practice problem/15jul/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using _15jul.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _15jul.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
