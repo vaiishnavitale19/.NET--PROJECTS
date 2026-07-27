03014d273cab93a4a8d2e1bb1cb666814779502b
vaiishnavitale19 <vaiishnavitale19@gmail.com>
2026-07-27 11:49:46 +0530

    Add files via upload

---
+++ .NET practice problem/25July/Controllers/StudentController.cs
@@ -0,0 +1,96 @@
+﻿using _25July.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _25July.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private static List<Student> students = new()
+        {
+            new Student
+            {
+                Id = 1,
+                Name = "Kartik",
+                Age = 18,
+                Department = "CSE"
+            },
+
+            new Student
+            {
+                Id = 2,
+                Name = "Ram",
+                Age = 19,
+                Department = "IT"
+            },
+
+            new Student
+            {
+                Id = 3,
+                Name = "Pratik",
+                Age = 20,
+                Department = "EXTC"
+            }
+        };
+
+        // GET: api/student
+        [HttpGet]
+        public IActionResult GetStudent()
+        {
+            return Ok(students);
+        }
+
+        // GET: api/student/2
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+                return NotFound();
+
+            return Ok(student);
+        }
+
+        // POST: api/student
+        [HttpPost]
+        public IActionResult AddStudent(Student student)
+        {
+            students.Add(student);
+
+            return CreatedAtAction(nameof(GetStudent), new { id = student.Id }, student);
+        }
+
+        // PUT: api/student/2
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, Student updateStudent)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+                return NotFound();
+
+            student.Name = updateStudent.Name;
+            student.Age = updateStudent.Age;
+            student.Department = updateStudent.Department;
+
+            return NoContent();
+        }
+
+        // DELETE: api/student/2
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            var student = students.FirstOrDefault(s => s.Id == id);
+
+            if (student == null)
+                return NotFound();
+
+            students.Remove(student);
+
+            return NoContent();
+        }
+    }
+}
---
+++ .NET practice problem/25July/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace _25July.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Department { get; set; }
+    }
+}
