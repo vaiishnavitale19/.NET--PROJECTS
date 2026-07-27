05578e5ab28526d850bd156dba3c0a2ccbf2b598
vaiishnavitale19 <vaiishnavitale19@gmail.com>
2026-07-27 11:54:53 +0530

    Add files via upload

---
+++ .NET practice problem/27July/Controllers/EmployeeController.cs
@@ -0,0 +1,110 @@
+﻿using _27July.Controllers.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _27July.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        static List<Employee> employees = new List<Employee>()
+        {
+            
+          new Employee() { Id = 101, Name = "Mamta", LastName = "B", Dept = "IT", PhoneNum = 789654 },
+          new Employee() { Id = 102, Name = "John", LastName = "Amit", Dept = "IT", PhoneNum = 890654 },
+          new Employee() { Id = 103, Name = "Bob", LastName = "Alice", Dept = "Admin", PhoneNum = 700054 },
+            
+        };
+
+        //get all employee list
+        [HttpGet]
+        public IActionResult getEmployee()
+        {
+            return Ok(employees);   // Ok - 200
+        }
+
+        //get employee by id
+        [HttpGet("{id}")]
+        public IActionResult getEmployeeById(int id)
+        {
+            var employee = employees.FirstOrDefault(x => x.Id == id);
+
+            if (employee == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(employee);
+        }
+
+        //add new employee record
+        [HttpPost]
+        public IActionResult AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+
+            return Ok(employee);
+        }
+        // Edit employee record
+        [HttpPut("{id}")]
+        public IActionResult UpdateEmployee(int id, Employee employee)
+        {
+            var employee1 = employees.FirstOrDefault(x => x.Id == id);
+
+            if (employee1 == null)
+            {
+                return NotFound();
+            }
+
+            employee1.LastName = employee.LastName;
+
+            return Ok(employee1);
+        }
+
+
+        // Get employees by department
+        [HttpGet("Dept/{dept}")]
+        public IActionResult GetEmployeeByDept(string dept)
+        {
+            var result = employees.Where(s => s.Dept.Equals(dept, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (!result.Any())
+            {
+                return NotFound("No employee found under this department.");
+            }
+
+            return Ok(result);
+        }
+        // Get employee by name
+        [HttpGet("Name/{name}")]
+        public IActionResult GetEmployeeByName(string name)
+        {
+            var result = employees.Where(s => s.Name.Equals(name, StringComparison.OrdinalIgnoreCase)).ToList();
+
+            if (!result.Any())
+            {
+                return NotFound("No employee found with this name.");
+            }
+
+            return Ok(result);
+        }
+        // Delete employee record
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+            var employee1 = employees.FirstOrDefault(x => x.Id == id);
+
+            if (employee1 == null)
+            {
+                return NotFound();
+            }
+
+            employees.Remove(employee1);
+
+            return Ok(employee1);
+        }
+
+
+    }
+}
---
+++ .NET practice problem/27July/Models/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _27July.Controllers.Model
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Emp Id is required")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Emp name is required")]
+        [StringLength(25, MinimumLength = 3, ErrorMessage = "Must be at least 3 leater")]
+        public string Name { get; set; }
+
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Emp Dept is required")]
+        [StringLength(25, ErrorMessage = "dept cannot be more than 25 letter")]
+        public string Dept { get; set; }
+
+        [Required(ErrorMessage = "Emp PhoneNUM is required")]
+        public long PhoneNum { get; set; }
+    }
+}
