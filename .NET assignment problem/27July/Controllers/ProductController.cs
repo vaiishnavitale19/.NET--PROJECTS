24993b6c1e8c0e30f6eab4fe23ba95545ea470ca
vaiishnavitale19 <vaiishnavitale19@gmail.com>
2026-07-28 17:25:25 +0530

    Add files via upload

---
+++ .NET assignment problem/27July/Controllers/ProductController.cs
@@ -0,0 +1,99 @@
+﻿using _27July_Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _27July_Assignment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new()
+        {
+            new Product
+            {
+                Id = 1,
+                Name = "Laptop",
+                Price = 65000,
+                Category = "Electronics"
+            },
+
+            new Product
+            {
+                Id = 2,
+                Name = "Mobile",
+                Price = 25000,
+                Category = "Electronics"
+            },
+
+            new Product
+            {
+                Id = 3,
+                Name = "Shoes",
+                Price = 3000,
+                Category = "Fashion"
+            }
+        };
+
+        // GET : api/product
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(products);
+        }
+
+        // GET : api/product/1
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        // POST : api/product
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            products.Add(product);
+
+            return CreatedAtAction(
+                nameof(GetProduct),
+                new { id = product.Id },
+                product
+            );
+        }
+
+        // PUT : api/product/1
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, Product updatedProduct)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            product.Name = updatedProduct.Name;
+            product.Price = updatedProduct.Price;
+            product.Category = updatedProduct.Category;
+
+            return NoContent();
+        }
+
+        // DELETE : api/product/1
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            products.Remove(product);
+
+            return NoContent();
+        }
+    }
+}
---
+++ .NET assignment problem/27July/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace _27July_Assignment.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Category { get; set; }
+    }
+}
