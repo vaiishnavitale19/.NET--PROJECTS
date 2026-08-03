455800f2e90af3b05f008ed3e94962df29b07200
vaiishnavitale19 <vaiishnavitale19@gmail.com>
2026-08-03 11:12:47 +0530

    Add files via upload

---
+++ .NET assignment problem/7July/7jui1problem/problem1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class problem1
+{
+    static void Main()
+    {
+        int totalPackages = 0;
+        int qualityCheck = 0;
+        int priorityShipment = 0;
+        int normalProcessing = 0;
+
+        for (int packageId = 1001; packageId <= 1020; packageId++)
+        {
+            totalPackages++;
+
+            if (packageId % 4 == 0)
+            {
+                Console.WriteLine("Package ID: " + packageId + " - Quality Check Required");
+                qualityCheck++;
+            }
+            else if (packageId % 5 == 0)
+            {
+                Console.WriteLine("Package ID: " + packageId + " - Priority Shipment");
+                priorityShipment++;
+            }
+            else
+            {
+                Console.WriteLine("Package ID: " + packageId + " - Normal Processing");
+                normalProcessing++;
+            }
+        }
+
+        Console.WriteLine("Total Packages Processed: " + totalPackages);
+        Console.WriteLine("Packages Requiring Quality Check: " + qualityCheck);
+        Console.WriteLine("Priority Shipments: " + priorityShipment);
+        Console.WriteLine("Normal Packages: " + normalProcessing);
+    }
+}
---
+++ .NET assignment problem/7July/7jui2problem/problem2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class problem2
+{
+    static void Main()
+    {
+        int totalPower = 0;
+        int maintenance = 0;
+        int normal = 0;
+        int efficient = 0;
+
+        for (int lightNumber = 1; lightNumber <= 30; lightNumber++)
+        {
+            int power = 80 + (lightNumber * 5);
+            totalPower += power;
+
+            Console.Write("Light " + lightNumber + " : Power = " + power + " W - ");
+
+            if (power > 180)
+            {
+                Console.WriteLine("Maintenance Required");
+                maintenance++;
+            }
+            else if (power >= 140 && power <= 180)
+            {
+                Console.WriteLine("Normal Operation");
+                normal++;
+            }
+            else
+            {
+                Console.WriteLine("Energy Efficient");
+                efficient++;
+            }
+        }
+
+        double averagePower = (double)totalPower / 30;
+
+        Console.WriteLine("Total Power Consumed: " + totalPower + " W");
+        Console.WriteLine("Average Power Consumption: " + averagePower + " W");
+        Console.WriteLine("Maintenance Required Lights: " + maintenance);
+        Console.WriteLine("Normal Operation Lights: " + normal);
+        Console.WriteLine("Energy Efficient Lights: " + efficient);
+    }
+}
